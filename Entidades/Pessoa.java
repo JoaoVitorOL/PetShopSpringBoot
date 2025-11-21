8788a0a814bfd3f14186e55568507091455328f2
WittBrasil <111887823+WittBrasil@users.noreply.github.com>
2025-11-21 15:49:51 -0300

    Update Pessoa.java

--- Entidades/Pessoa.java
+++ Entidades/Pessoa.java
@@ -1,10 +1,9 @@
 package com.petshop.demo.domain.entities;
 
 import jakarta.persistence.*;
-import java.io.Serializable;
 
 @MappedSuperclass
-public abstract class Pessoa implements Serializable {
+public abstract class Pessoa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,7 +12,8 @@ public abstract class Pessoa implements Serializable {
     @Column(nullable = false)
     private String nome;
 
-    private String sobrenome
+    @Column(nullable = false)
+    private String sobrenome;
 
     @Column(unique = true, nullable = false)
     private String cpf;
@@ -24,53 +24,28 @@ public abstract class Pessoa implements Serializable {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    private String senha;
+
     // Getters e Setters
-    public Integer getId() {
-        return id;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public String getNome() {
-        return nome;
-    }
+    public String getSobrenome() { return sobrenome; }
+    public void setSobrenome(String sobrenome) { this.sobrenome = sobrenome; }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
 
-    public String getSobrenome() {
-        return sobrenome;
-    }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
 }
