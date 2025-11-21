088c35e6b408657dab2fddf983cecef9814e0574
WittBrasil <111887823+WittBrasil@users.noreply.github.com>
2025-11-21 15:38:00 -0300

    Update Veterinario.java

--- Entidades/Veterinario.java
+++ Entidades/Veterinario.java
@@ -1,5 +1,37 @@
 package com.example.petshop.entidades;
 
-public class Veterinario {
-    
+import jakarta.persistence.*;
+
+@Entity
+public class Veterinario extends Pessoa {
+
+    @Column(nullable = false)
+    private Double salario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Especialidade especialidade;
+
+    public Veterinario() {}
+
+    public Veterinario(Double salario, Especialidade especialidade) {
+        this.salario = salario;
+        this.especialidade = especialidade;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
 }
