20d037f77131b79cd65f74cbc55b1c2ebd26dcd0
João Vitor de Oliveira Lima <160860021+JOHNcoding9@users.noreply.github.com>
2025-11-21 14:57:03 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,85 @@
+package com.petshop.demo.domain.entities;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+//import com.example.demo.domain.enums.Perfil;
+
+@MappedSuperclass
+public abstract class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private string sobrenome
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(unique = true, nullable = false)
+    private String telefone;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column( nullable = false)
+    private String email;
+
+
+    // Getters e Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String nome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
