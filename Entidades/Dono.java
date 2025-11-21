e637aac927760e2d206e3e139c8ec7b3c66977f7
WittBrasil <111887823+WittBrasil@users.noreply.github.com>
2025-11-21 15:26:49 -0300

    Update Dono.java

--- Entidades/Dono.java
+++ Entidades/Dono.java
@@ -1,5 +1,29 @@
-package com.example.petshop.entidades;
+package NOME DO PACOTE;
 
-public class Dono {
+import jakarta.persistence.*;
+
+@Entity
+public class Dono extends Pessoa {
     
+    @Enumerated(EnumType.STRING)
+    private Cidade cidade;
+
+    private String endereco;
+
+    // Getters e setters
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
