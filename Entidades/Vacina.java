97e37e7d2cdfdb62f76aac9e28f52180475183e3
WittBrasil <111887823+WittBrasil@users.noreply.github.com>
2025-11-21 16:06:09 -0300

    Update Vacina.java

--- Entidades/Vacina.java
+++ Entidades/Vacina.java
@@ -1,5 +1,33 @@
 package com.example.petshop.entidades;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Vacina {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idVacina;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private Integer duracao;
+
+    // Getters e Setters
+    public Integer getIdVacina() { return idVacina; }
+    public void setIdVacina(Integer idVacina) { this.idVacina = idVacina; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public Integer getDuracao() { return duracao; }
+    public void setDuracao(Integer duracao) { this.duracao = duracao; }
 }
